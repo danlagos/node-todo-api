0290e0dec61bedfd62dc7a95bb74b3ff50410bee
Daniel Lagos <lagos.d@gmail.com>
2020-01-21 19:50:39 -0800

    add dummy data to connect to a database.

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router()
 
-app.get('/', (req, res) => res.send('Hello World'));
+router.get('/', (req, res) => res.send('Hello World'));
+
+router.route('/todos')
+  .get((req, res) => {
+    res.json([
+              {
+                "_id": "a",
+                "text" : "Item 1",
+                "done" : false
+              },{
+                "_id": "b",
+                "text" : "Item 2",
+                "done" : false
+              },{
+                "_id": "c",
+                "text" : "Item 3",
+                "done" : true
+              },{
+                "_id": "d",
+                "text" : "Item 4",
+                "done" : false
+              }
+            ]);
+  })
+
+app.use('/', router);
 
 app.listen(port, () => console.log(`Example app is listening on port ${port}`));
